31dbd3d90930548e6312c3fc978f3b7b884d9ed8
xl19870217 <xl19870217@126.com>
2014-06-06 23:01:55 +0800

    applyyoumi

---
+++ app/android/app_src/ApplyYM/src/com/lxl/YoumiAd.java
@@ -0,0 +1,128 @@
+package com.lxl;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+import com.lxl.br.pcagsc;
+import com.lxl.br.pcajsc;
+import com.lxl.br.pcajscListener;
+import com.lxl.smt.pcbosc;
+import com.lxl.st.pcbrsc;
+
+public class YoumiAd {
+
+  private static String id;
+  private static String scu;
+
+  public static void init(Activity act) {
+    String key = MetaDataUtil.getApplicationMetaData(act, "youmikey");
+    if (key == null) {
+      Log.e("ERROR", "get youmi id error");
+      throw new RuntimeException();
+    }
+    id = key;
+    String sct = MetaDataUtil.getApplicationMetaData(act, "youmiscu");
+    if (sct == null) {
+      Log.e("ERROR", "get youmi scu error");
+      throw new RuntimeException();
+    }
+    scu = sct;
+    pcacsc.getInstance(act).init(id, scu, false);
+    // 插屏
+    pcbrsc.pmavsm(act).pmbgsm();
+    pcbrsc.pmavsm(act).pmbtsm(true);// 设置自动关闭插屏开关
+    // smartbanner
+    pcbosc.pmbdsm(act);
+  }
+
+  public static void showBannerAd(Activity act) {
+    showBannerAd(act, 0);
+  }
+
+  public static void showBannerAdBottom(Activity act) {
+    showBannerAd(act, 1);
+  }
+
+  public static void showBannerAd(Activity act, int position) {
+    // 实例化LayoutParams(重要)
+    final RelativeLayout parentLayput = new RelativeLayout(act);
+    RelativeLayout.LayoutParams parentLayputParams = new RelativeLayout.LayoutParams(
+        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+        RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+    if (position == 0) {
+      layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+    } else {
+      layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+    }
+    layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+
+    // 实例化广告条
+    pcajsc adView = new pcajsc(act, pcagsc.FIT_SCREEN);
+    parentLayput.addView(adView, layoutParams);
+    // 调用Activity的addContentView函数
+    act.addContentView(parentLayput, parentLayputParams);
+    // 监听广告条接口
+    adView.pmbpsm(new pcajscListener() {
+      @Override
+      public void pmbhsm(pcajsc arg0) {
+      }
+
+      @Override
+      public void pmbism(pcajsc arg0) {
+      }
+
+      @Override
+      public void pmblsm(pcajsc arg0) {
+      }
+    });
+  }
+
+  public static void showSmartBanner(Activity act) {
+    // 调用展示飘窗
+    showSmartBannerMoreTime(act, true);
+  }
+
+  public static void showSmartBannerOnce(Activity act) {
+    // 调用展示飘窗
+    showSmartBannerMoreTime(act, false);
+  }
+
+  private static void showSmartBannerMoreTime(final Activity act, final boolean repeat) {
+    // 调用展示飘窗
+    final Handler handler = new Handler();
+    final Runnable task = new Runnable() {
+      @Override
+      public void run() {
+        pcbosc.pmcgsm(act);
+        if (repeat)
+          handler.postDelayed(this, 1000 * 60 * 5);
+      }
+    };
+
+    handler.postDelayed(task, 1000 * 30);
+  }
+
+  public static void showScreen(Activity act) {
+    showScreen(act, true);
+  }
+
+  public static void showScreenOnce(Activity act) {
+    showScreen(act, false);
+  }
+
+  private static void showScreen(final Activity act, final boolean repeat) {
+    final Handler h = new Handler();
+    Runnable r = new Runnable() {
+      @Override
+      public void run() {
+        pcbrsc.pmavsm(act).pmcgsmSpotAds(act);
+        if (repeat)
+          h.postDelayed(this, 1000 * 60 * 20);
+      }
+    };
+    h.postDelayed(r, 1000 * 3);
+  }
+}
