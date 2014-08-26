ebe46bf87561d5365f5365dd9815f993d0a61631
xl19870217 <xl19870217@126.com>
2014-08-26 17:33:24 +0800

    v

--- app/android/app_src/GoogleAds/src/com/gad/GAD.java
+++ app/android/app_src/GoogleAds/src/com/gad/GAD.java
@@ -5,8 +5,8 @@ import android.os.Handler;
 
 public class GAD {
 
-  public static String UID_BANNER = "ca-app-pub-8002125689398145/6833106918";
-  public static String UID_COVER = "ca-app-pub-8002125689398145/8030638516";
+  public static String UID_BANNER = "ca-app-pub-2342479172622730/1042596003";
+  public static String UID_COVER = "ca-app-pub-2342479172622730/2519329202";
 
   public static Handler handler = new Handler();
 
