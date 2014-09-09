ed265143b7f3e2a2b31477adf4c1bbbab0de02b4
xl19870217 <xl19870217@126.com>
2014-09-09 14:01:25 +0800

    v

--- app/android/app_src/AllInOneSDK/src/com/cdd/utils/DateUtil.java
+++ app/android/app_src/AllInOneSDK/src/com/cdd/utils/DateUtil.java
@@ -17,6 +17,7 @@ public class DateUtil {
 				try {
 					URL url = new URL("http://www.baidu.com");// 取得资源对象
 					URLConnection uc = url.openConnection();// 生成连接对象
+					uc.setUseCaches(false);
 					uc.connect(); // 发出连接
 					long date = uc.getDate(); // 取得网站日期时间
 					if (date > 0)
